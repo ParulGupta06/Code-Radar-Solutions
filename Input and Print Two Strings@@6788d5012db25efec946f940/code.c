141b5e3830e18f660a973cce0bc73c838081d689
PARUL GUPTA <parulgupta6200@gmail.com>
2025-02-06 15:28:04 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -3,7 +3,7 @@ int main(){
     char str1[100];
     char str2[100];
     scanf("%s %S",&str1, str2);
-    printf("You entered:", str1\f str2)
+    printf("You entered: %s,%s", str1, str2)
 
    return 0;
 }
