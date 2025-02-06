dfce429a3b914fc83706c5651d22c0d6126cf466
PARUL GUPTA <parulgupta6200@gmail.com>
2025-02-06 15:49:59 +0530

    Solution to Input and Print a Double

--- Input and Print a Double@@6788d5012db25efec946f942/code.c
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -2,6 +2,6 @@
 int main(){
     float num1, num2;
     scanf("%f %f", &num1, &num2);  // Correct usage of & for both num1 and num2
-    printf("You entered: %4f %f\n", num1, num2);  // Use a comma, not & for separating variables
+    printf("You entered: %2f %f\n", num1, num2);  // Use a comma, not & for separating variables
     return 0;
 }
