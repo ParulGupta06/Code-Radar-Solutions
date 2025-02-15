9dc1bae55bfc4594346711f14d3a6cf6c47ee93f
PARUL GUPTA <parulgupta6200@gmail.com>
2025-02-15 14:15:07 +0530

    Solution to Check Divisibility

---
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int num;
+    num = int(input("Enter a number: "))
+if num % 5 == 0 and num % 11 == 0:
+    print("Divisible")
+else:
+    print("Not Divisible")
+
+}
